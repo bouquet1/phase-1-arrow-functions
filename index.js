6b145feaa065f64d27644d10886596f87f748a46
bouquet1 <bukuleta@gmail.com>
2023-03-28 11:51:07 -0700

    complete arrow functions

---
+++ index.js
@@ -0,0 +1,22 @@
+// const nums = [1,2,3];
+// const squares = nums.map(x = x**2);
+// squares; [1, 4, 9]
+// nums; [1, 2, 3]
+
+// In the following example, we can imagine that overdueTodoItems is a collection of DOM elements:
+//finishedItems = overdueToDoItems.map(item => item.className = "complete");
+// headers.innerText = `You finished ${finishedItems.length} items!`;
+
+//Or we might use map in billing software:
+//lapsedUserAccounts.map( u => sendBillTo(u.address) );
+
+const divide = function(num1, num2) {
+    return num1/num2;
+}
+divide(2000, 100);
+
+const square = x => x * x;
+
+const add = (x, y) => x + y;
+
+
